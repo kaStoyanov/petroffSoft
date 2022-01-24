b8e561b30a6fb64a59c9583efc7dbe0c991c9ff6
Kaloyan <eczenn@gmail.com>
2022-01-24 14:50:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
+import { useEffect, react, useState } from 'react'
+
+
 
 function App() {
+  const url = 'https://api.zippopotam.us/us/'
+  const handleInput = (e) => {
+    e.preventDefault()
+    let arr = e.target.value.split(',')
+    console.log(arr)
+    setZip(arr)
+
+  }
+  let multipleZips = []
+  const submit = async () => {
+    try {
+      zip.forEach(async (e) => {
+        console.log(e)
+        await axios.get(url + e).then((response) => {
+          console.log(e)
+          response.data.places.forEach((e) => {
+            multipleZips.push(`https://www.google.com/maps/search/?api=1&query=${e.latitude},${e.longitude}`)
+            console.log(multipleZips)
+          })
+        });
+        setTimeout(() => {
+          setZips(multipleZips)
+        }, 150);
+
+        console.log(zips)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const [zip, setZip] = useState();
+  const [zips, setZips] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header" >
+      <input onChange={handleInput} ></input>
+      <button style={{ width: '150px', margin: '15px' }} onClick={submit}>Submit</button>
+      <div >
+        {
+          zips.map((e, i) => {
+            return <li key={i}>
+              <a style={{ color: 'white' }} href={e}>Google Endpoint</a>
+            </li>
+          })
+
+        }
+
+      </div>
     </div>
+
   );
 }
 
